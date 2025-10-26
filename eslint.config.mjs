fc3cb0824bb020dae32d0b93675cdf6855c1f78e
tsukinuii <sasithonnuii@gmail.com>
2025-10-26 14:57:58 +0700

    [del] storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -20,6 +17,6 @@ const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript
     "build/**",
     "next-env.d.ts",
   ],
-}, ...storybook.configs["flat/recommended"]];
+}];
 
 export default eslintConfig;
